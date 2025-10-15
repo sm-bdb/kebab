6e1ed35fb77cdcff202943b18b0ae3150a8f2323
dirk <>
2025-10-15 14:54:00 +0000

    Views toegevoegd voor gemiddelde beoordelingen per restaurant

--- database/schema.sql
+++ database/schema.sql
@@ -44,3 +44,59 @@ CREATE INDEX idx_reviews_restaurant_id ON reviews(restaurant_id);
 CREATE INDEX idx_review_details_review_id ON review_details(review_id);
 CREATE INDEX idx_review_details_criterium_id ON review_details(criterium_id);
 CREATE INDEX idx_criteria_theme_id ON criteria(theme_id);
+
+-- View to calculate average rating per criterium per restaurant
+CREATE OR REPLACE VIEW restaurant_criterium_averages AS
+SELECT
+    r.id AS restaurant_id,
+    r.name AS restaurant_name,
+    c.id AS criterium_id,
+    c.name AS criterium_name,
+    t.id AS theme_id,
+    t.name AS theme_name,
+    ROUND(AVG(rd.rating), 1) AS average_rating
+FROM restaurants r
+CROSS JOIN criteria c
+LEFT JOIN themes t ON c.theme_id = t.id
+LEFT JOIN reviews rev ON rev.restaurant_id = r.id
+LEFT JOIN review_details rd ON rd.review_id = rev.id AND rd.criterium_id = c.id
+GROUP BY r.id, r.name, c.id, c.name, t.id, t.name;
+
+-- View to calculate total average rating per restaurant (across all criteria)
+CREATE OR REPLACE VIEW restaurant_total_averages AS
+SELECT
+    r.id AS restaurant_id,
+    r.name AS restaurant_name,
+    ROUND(AVG(rd.rating), 1) AS average_rating,
+    json_agg(
+        jsonb_build_object(
+            'id', rev.id,
+            'username', rev.username,
+            'comment', rev.comment,
+            'review_date', rev.review_date
+        )
+    ) FILTER (WHERE rev.comment IS NOT NULL AND rev.comment != '') AS comments
+FROM restaurants r
+LEFT JOIN reviews rev ON rev.restaurant_id = r.id
+LEFT JOIN review_details rd ON rd.review_id = rev.id
+GROUP BY r.id, r.name;
+
+-- Combined view with restaurant info, total average, criterium averages, and comments as JSON
+CREATE OR REPLACE VIEW restaurant_averages_combined AS
+SELECT
+    rta.restaurant_id,
+    rta.restaurant_name,
+    rta.average_rating,
+    rta.comments,
+    json_agg(
+        jsonb_build_object(
+            'criterium_id', rca.criterium_id,
+            'criterium_name', rca.criterium_name,
+            'theme_id', rca.theme_id,
+            'theme_name', rca.theme_name,
+            'average_rating', rca.average_rating
+        )
+    ) AS criterium_averages
+FROM restaurant_total_averages rta
+LEFT JOIN restaurant_criterium_averages rca ON rta.restaurant_id = rca.restaurant_id
+GROUP BY rta.restaurant_id, rta.restaurant_name, rta.average_rating, rta.comments;
